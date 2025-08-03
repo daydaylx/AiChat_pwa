337cd422c41ea594677096034c659002ddbbe640
dlx <daydayfay05@gmail.com>
2025-08-03 19:58:00 +0200

    fix: remove base path for Vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/AiChat_pwa/', // <-- exakt Repo-Name, Slash am Ende!
   plugins: [react()],
 });
